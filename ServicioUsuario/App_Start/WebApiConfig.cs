ab1574c4508ecad02e6942bed92ab28884ad1a9a
fede_zurbriggen <fedezurbriggen@gmail.com>
2017-11-14 23:05:08 -0300

    Solucion final.

--- ServicioUsuario/App_Start/WebApiConfig.cs
+++ ServicioUsuario/App_Start/WebApiConfig.cs
@@ -10,6 +10,9 @@ namespace ServicioUsuario
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
@@ -19,6 +22,8 @@ namespace ServicioUsuario
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
