cab739a830f4cb0b58a1bae8d29eac8eaf0e22db
fede_zurbriggen <fedezurbriggen@gmail.com>
2017-11-28 18:57:36 -0300

    nueva clase Perdidas

---
+++ ServicioUsuario/Controllers/PerdidasController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ServicioUsuario.Datos;
+
+namespace ServicioUsuario.Controllers
+{
+    public class PerdidasController : ApiController
+    {
+        private RafaelaDenunciaEntities db = new RafaelaDenunciaEntities();
+
+        // GET: api/Perdidas
+        public List<Perdidas> GetPerdidas()
+        {
+            var result = db.Perdidas.ToList();
+            return result;
+        }
+
+        // GET: api/Perdidas/5
+        [ResponseType(typeof(Perdidas))]
+        public IHttpActionResult GetPerdidas(int id)
+        {
+            Perdidas perdidas = db.Perdidas.Find(id);
+            if (perdidas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(perdidas);
+        }
+
+        // PUT: api/Perdidas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPerdidas(int id, Perdidas perdidas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != perdidas.idPerdida)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(perdidas).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PerdidasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Perdidas
+        [ResponseType(typeof(Perdidas))]
+        public IHttpActionResult PostPerdidas(Perdidas perdidas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Perdidas.Add(perdidas);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = perdidas.idPerdida }, perdidas);
+        }
+
+        // DELETE: api/Perdidas/5
+        [ResponseType(typeof(Perdidas))]
+        public IHttpActionResult DeletePerdidas(int id)
+        {
+            Perdidas perdidas = db.Perdidas.Find(id);
+            if (perdidas == null)
+            {
+                return NotFound();
+            }
+
+            db.Perdidas.Remove(perdidas);
+            db.SaveChanges();
+
+            return Ok(perdidas);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PerdidasExists(int id)
+        {
+            return db.Perdidas.Count(e => e.idPerdida == id) > 0;
+        }
+    }
+}
--- ServicioUsuario/Datos/RafaelaDenunciaModel.Context.cs
+++ ServicioUsuario/Datos/RafaelaDenunciaModel.Context.cs
@@ -29,5 +29,6 @@ namespace ServicioUsuario.Datos
         public virtual DbSet<Denuncias> Denuncias { get; set; }
         public virtual DbSet<TipoDenuncias> TipoDenuncias { get; set; }
         public virtual DbSet<Usuarios> Usuarios { get; set; }
+        public virtual DbSet<Perdidas> Perdidas { get; set; }
     }
 }
